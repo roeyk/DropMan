0300c2f2371d20cc32105df8a084469f14a7cf53
Roey Katz <roey.katz@gmail.com>
2026-06-21 16:50:45 -0400

    Bump KWin profile config script marker

--- kwin/dropman/contents/code/main.js
+++ kwin/dropman/contents/code/main.js
@@ -12,7 +12,7 @@
 */
 
 const LOG_PREFIX = "dropman: ";
-const SCRIPT_VERSION = "preserve-geometry-current-desktop-20260621";
+const SCRIPT_VERSION = "profile-config-20260621";
 
 const DEFAULT_CONFIG = {
     bindings: [
